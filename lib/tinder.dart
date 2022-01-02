9ee4eac7efed4f973e71b4b3477e3d020d663ec6
Wagner-Coche <wagnercoche@gmail.com>
2022-01-03 00:58:23 +0100

    updated file tinder.dart

--- lib/tinder.dart
+++ lib/tinder.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Tinder extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
     
-    return SizedBox();
+    return SizedBox(
+      child: Scaffold(
+        body: SizedBox(
+          child: Stack(
+            children: [
+              Container(
+                child: Theme(),
+              ),
+              Container(
+                child: Img(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+class Theme extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      color: Color(0xFFff7585),
+    );
+  }
+
+}
+
+class Img extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return SizedBox(
+      child: Image.asset(
+        "assets/images/tinder3.jpg"
+      ),
+    );
   }
 
 }

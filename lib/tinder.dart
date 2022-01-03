6b2ec63660eebce8d8cc5b51366ef61fde8d8d53
Wagner-Coche <wagnercoche@gmail.com>
2022-01-03 03:04:09 +0100

    final commit

--- lib/tinder.dart
+++ lib/tinder.dart
@@ -8,14 +8,84 @@ class Tinder extends StatelessWidget{
     
     return SizedBox(
       child: Scaffold(
-        body: SizedBox(
-          child: Stack(
+        body: Container(
+          color: Color(0xFFff7585),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                child: Theme(),
+                height: 150,
+                child: Align(
+                  child: Image.asset(
+                    "assets/images/tinder.png",
+                    color: Colors.white,
+                  ),
+                ),
               ),
               Container(
-                child: Img(),
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                ),
+              ),
+              Container(
+                child: Text(
+                  "Location Changer",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                ),
+              ),
+              Container(
+                child: Text(
+                  "Plugin app for Tinder",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(22),
+                ),
+              ),
+              Container(
+                height: 60,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    right: 105,
+                    left: 105,
+                  ),
+                  child: ElevatedButton(
+                    onPressed: (){
+                      
+                    },
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Login with Facebook",
+                        style: TextStyle(
+                          color: Color(0xFFff7585),
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      primary: Colors.white,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -26,28 +96,3 @@ class Tinder extends StatelessWidget{
 
 }
 
-class Theme extends StatelessWidget{
-  
-  @override
-  Widget build(BuildContext context) {
-    
-    return Container(
-      color: Color(0xFFff7585),
-    );
-  }
-
-}
-
-class Img extends StatelessWidget{
-  
-  @override
-  Widget build(BuildContext context) {
-    
-    return SizedBox(
-      child: Image.asset(
-        "assets/images/tinder3.jpg"
-      ),
-    );
-  }
-
-}
